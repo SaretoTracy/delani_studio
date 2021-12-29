bbec080d727adbbf31a43cde989b3073e3fea86f
saretotracy <tracysareto@gmail.com>
2021-12-29 19:18:00 +0300

    modify loop

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const show = document.querySelectorAll(".show");
 const pop = document.querySelectorAll(".pop");
 
-for (let i = 0; i < show.length; i++) {
+for (let i = 0; i < show.length || i < pop.length; i++) {
     show[i].addEventListener("click", (e) => {
         show[i].classList.remove("display");
+        pop[i].classList.add("display")
     })
+    pop[i].addEventListener("click", (e) => {
+        pop[i].classList.remove("display");
+        show[i].classList.add("display")
+    })
+
 }
 
 
